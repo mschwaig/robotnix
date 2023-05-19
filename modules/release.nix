d33a7ceb955a95f82a86627838f95dfd11668328
Martin Schwaighofer <mschwaig@users.noreply.github.com>
2023-05-19 13:15:50 +0200

    fix verification

--- modules/release.nix
+++ modules/release.nix
@@ -151,8 +151,8 @@ in
     # These can be used to build these products inside nix. Requires putting the secret keys under /keys in the sandbox
     unsignedTargetFiles = config.build.android + "/${config.productName}-target_files-${config.buildNumber}.zip";
     signedTargetFiles = runWrappedCommand "signed_target_files" signedTargetFilesScript { targetFiles=unsignedTargetFiles;};
-    verifiedTargetFiles = runWrappedCommandVerify "verifieded_target_files" verifiedTargetFilesScript {
-      inherit signedTargetFiles unsignedTargetFiles; } signedTargetFiles;
+    verifiedTargetFiles = runWrappedCommandKeyless "verifieded_target_files" verifiedTargetFilesScript {
+      inherit signedTargetFiles unsignedTargetFiles; };
     targetFiles = if config.signing.enable then verifiedTargetFiles else unsignedTargetFiles;
     ota = runWrappedCommand "ota_update" otaScript { inherit targetFiles; };
     incrementalOta = runWrappedCommand "incremental-${config.prevBuildNumber}" otaScript { inherit targetFiles; inherit (config) prevTargetFiles; };
